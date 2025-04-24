894af98affb7f8e292116fdcde5ee5b13c6b9719
Parsasohi <parsa86tahmasebi86@gmail.com>
2025-04-24 15:13:33 +0330

    developed

--- task-tracker.py
+++ task-tracker.py
@@ -1,29 +1,40 @@
 import json
 import os
+import datetime
 
 tasks = []
+print("--------------\nwelcome to task tracker io\nplease choose an action\n")
 
 if os.path.exists("tasks.json"):
     with open("tasks.json", 'r') as file:
         tasks = json.load(file)
+def start():
+  print("--------------")
+  print("1- Add task")
+  print("2- remove task")
+  print("3- list all tasks")
+  print("4- update task")
+  print("5- to quit")
+  action = int(input("\nenter number of your desired action: "))
+  return action
 
-print("--------------\nwelcome to task tracker io\nplease choose an action\n")
-print("1- Add task")
-print("2- remove task")
-print("3- list all tasks")
-action = int(input("\nenter number of your desired action: "))
+action = start()
 
 def addTask():
     title = input("enter the title of your task: ")
-    new_id = len(tasks) + 1  # Generate new ID based on current task count
+    new_id = len(tasks) + 1 
+    time = datetime.datetime.now()
+    creationDate = f"{time.year}/{time.month}/{time.day}"
     new_task = {
         "title": title,
-        "id": new_id
+        "id": new_id,
+        "dateCreated" : creationDate
     }
     tasks.append(new_task)
     with open("tasks.json", 'w') as file:
         json.dump(tasks, file, indent=4)
     print(f"New task added: {title} (ID: {new_id})")
+    start()
 
 def removeTask():
     task_id = int(input("Enter the ID of the task to remove: "))
@@ -35,6 +46,7 @@ def removeTask():
             print(f"Task {task_id} removed successfully")
             return
     print(f"Task with ID {task_id} not found")
+    start()
 
 def listTasks():
     if not tasks:
@@ -43,6 +55,19 @@ def listTasks():
         print("\nCurrent Tasks:")
         for task in tasks:
             print(f"ID: {task['id']} - {task['title']}")
+    start()
+            
+def updateTasks():
+  task_id = int(input("Enter the ID of the task to update: "))
+  for task in tasks:
+    if task["id"] == task_id:
+      newTitle = input("Enter the new title: ")
+      task["title"] = newTitle
+      with open("tasks.json", 'w') as file:
+                json.dump(tasks, file, indent=4)
+      print(f"Task {task_id} updated successfully")
+      return
+  print(f"Task with ID {task_id} not found")
 
 match action:
     case 1:
@@ -51,5 +76,9 @@ match action:
         removeTask()
     case 3:
         listTasks()
+    case 4:
+        updateTasks()
+    case 5:
+        print("Bye!")
     case _:
         print("Invalid action")
